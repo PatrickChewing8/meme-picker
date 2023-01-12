0832c6d179abeaa9084124040fb238074c2b2dfb
PatrickChewing8 <84030379+PatrickChewing8@users.noreply.github.com>
2023-01-11 20:50:24 -0500

    Last commit

--- index.js
+++ index.js
@@ -1,30 +1,84 @@
 import { catsData } from '/data.js';
 
 const emotionRadios = document.getElementById('emotion-radios');
+const getImageBtn = document.getElementById('get-image-btn');
+const gifsOnlyOptions = document.getElementById('gifs-only-option');
+const memeModalInner = document.getElementById('meme-modal-inner');
+const memeModal = document.getElementById('meme-modal');
+const memeModalCoseBtn = document.getElementById('meme-modal-close-btn');
 
-emotionRadios.addEventListener("change", highlightCheckedOption)
+emotionRadios.addEventListener('change', highlightCheckedOption);
+
+memeModalCoseBtn.addEventListener('click', closeModal);
+
+getImageBtn.addEventListener('click', renderCat);
 
 function highlightCheckedOption(e) {
-    document.getElementById(e.target.id).parentElement.classList.add('highlight');
+	const radios = document.getElementsByClassName('radio');
+	for (let radio of radios) {
+		radio.classList.remove('highlight');
+	}
+	document.getElementById(e.target.id).parentElement.classList.add('highlight');
+}
+
+function closeModal() {
+	memeModal.style.display = 'none';
+}
+function renderCat() {
+	const catObject = getSingleCatObject();
+	memeModalInner.innerHTML = `<img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >`;
+	memeModal.style.display = 'flex';
+}
+
+function getMatchingCatsArray() {
+	if (document.querySelector('input[type = "radio"]:checked').value) {
+		const selectedEmotion = document.querySelector(
+			'input[type = "radio"]:checked',
+		).value;
+		const isGif = gifsOnlyOptions.checked;
+
+		const matchingCatsArray = catsData.filter(function (cat) {
+			if (isGif) {
+				return cat.emotionTags.includes(selectedEmotion) && cat.isGif;
+			} else {
+				return cat.emotionTags.includes(selectedEmotion);
+			}
+		});
+		return matchingCatsArray;
+	}
+}
+
+function getSingleCatObject() {
+	const catsArray = getMatchingCatsArray();
+	if (catsArray.length === 1) {
+		return catsArray[0];
+	} else {
+		const randomNumber = Math.floor(Math.random() * catsArray.length);
+		return catsArray[randomNumber];
+	}
 }
 
 function getEmotionsArray(cats) {
-    const emotionsArray = [];
-    for (let cat of cats) {
-        for (let emotion of cat.emotionTags) {
-            if (!emotionsArray.includes(emotion)) {
-                emotionsArray.push(emotion);
-            }
-        }
-    }
-    return emotionsArray;
+	const emotionsArray = [];
+	for (let cat of cats) {
+		for (let emotion of cat.emotionTags) {
+			if (!emotionsArray.includes(emotion)) {
+				emotionsArray.push(emotion);
+			}
+		}
+	}
+	return emotionsArray;
 }
 
 function renderEmotionsRadios(cats) {
-    let radioItems = ``;
-    const emotions = getEmotionsArray(cats);
-    for (let emotion of emotions) {
-        radioItems += `
+	let radioItems = ``;
+	const emotions = getEmotionsArray(cats);
+	for (let emotion of emotions) {
+		radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
             <input
@@ -35,8 +89,8 @@ function renderEmotionsRadios(cats) {
                 >
         </div>
         `;
-    }
-    emotionRadios.innerHTML = radioItems;
+	}
+	emotionRadios.innerHTML = radioItems;
 }
 
 renderEmotionsRadios(catsData);
